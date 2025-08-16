9a43dea91218efc01130d9b035eb6bd950c4aec7
Sanjana S Aithal <sanjanaaithal28@gmail.com>
2025-08-16 18:56:13 +0530

    TO check if an array is sorted

---
+++ Arrays/sorted.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+bool isSorted(int arr[], int n){
+    for(int i=1; i<n; i++){
+        if(arr[i]>=arr[i-1]){}
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<isSorted(arr, n);
+    
+}
